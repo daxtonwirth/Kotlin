0f70447fd99a14421506b900b215d9a58e5073a6
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-09-30 15:16:51 +0900

    Create Variables.kt

---
+++ Variables.kt
@@ -0,0 +1,11 @@
+fun main(args : Array<String>) {
+
+var name = "Dax" //string: var name: String = "Dax"
+val age = 22 //int: val age: Int = 23
+
+  println("My name is $name and I am $age years old")
+  
+name = "Kumiho" //var is mutable
+age = "24" //error
+  
+}
