7dae464ac0642c4bf2904096a5e99751d40475bb
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-10-12 09:07:20 +0900

    Update Variables.kt

--- Variables.kt
+++ Variables.kt
@@ -7,5 +7,16 @@ val age = 22 //int: val age: Int = 23
   
 name = "Kumiho" //var is mutable
 age = "24" //error
+ 
+// Expressions
+val score: Int
+score = 90 + 25
+  
+val a = 12
+val b = 13
+val max: Int
+
+max = if (a > b) a else b
+println("$max")
   
 }
