f07a5ec67fa62dfe8238c2e6402fb14f21ba5a0c
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-10-16 10:14:14 +0900

    Finishing touches

--- Password Manager.kt
+++ Password Manager.kt
@@ -1,37 +1,44 @@
 import java.io.File
 
+// Return what the user inputted
 fun getUserInput(): Int {
     println("Enter an option:\n1. View Passwords \n2. Add password\n3. Exit")
     print("Option: ")
     return Integer.valueOf(readLine())
 }
 
+// Show the user the websites they have passwords saved for
 fun viewWebsites(fileName : File) {
     println("Here are the current websites you have passwords saved for:")
     var websiteNames = fileName.readLines()
+    // Get only the website names
     for (x in websiteNames) {
         println(x.split("|")[0])
     }
     println()
 }
 
+// View the passwords the user has after inputting a website
 fun viewPasswords(fileName : File){
     print("Enter the website for the password: ")
     val newWeb = readLine().toString()
     var websiteNames = fileName.readLines()
     var found = 1
     for (x in websiteNames) {
+        // Split the user and password and display
         if (newWeb == x.split("|")[0]) {
             println("Username " + found + ": " + x.split("|")[1])
             println("Password " + found + ": " + x.split("|")[2])
             found += 1
         }
     }
+    // If the inputted website is not found, notify the user
     if (found == 1){
         println("Website " + newWeb + " is not saved")
     }
 }
 
+// Allow the user to add a password to their password file
 fun addPassword(fileName : File) {
     print("Enter the website: ")
     val newWeb = readLine()
@@ -39,16 +46,15 @@ fun addPassword(fileName : File) {
     val newUsername = readLine()
     print("Enter your new password: ")
     val newPassword = readLine()
+    // Append user info to file in format "WEBSITE|USERNAME|PASSWORD"
     fileName.appendText(newWeb.plus("|").plus(newUsername).plus("|").plus(newPassword).plus("\n"))
 }
 
-fun main() {
-    //add timeout for login
-    val userFileName = File("users.txt")
+// Class for user actions including creating an account, logging in, and having a session
+class User(){
 
-    print("Enter 1 to login or 2 to create an account or anything else to quit: ")
-    val userInput = readLine()
-    if (userInput == "2"){
+    // Creates the user's account and adds it to the file
+    fun createAccount(userFileName: File){
         print("Please enter your username: ")
         val username = readLine()
         print("Please enter your password: ") // add hashing
@@ -59,52 +65,78 @@ fun main() {
         println("Account Creation Successful")
     }
 
-    println("Login")
-    print("Please enter your username: ")
-    val username = readLine()
+    // Returns the user's password file if successful
+    fun login(userFileName: File): File {
+        println("Login")
+        print("Please enter your username: ")
+        val username = readLine()
+        var userNames = userFileName.readLines()
+        for (x in userNames) {
+            if (x.split("|")[0] == username) {
 
-    var userNames = userFileName.readLines()
-    var found = 0
-    for (x in userNames) {
-        if (x.split("|")[0] == username) {
-            found += 1
-
-            print("Password: ")
-            val password = readLine()
-            if (password == x.split("|")[1]) {
-                println("Login Successful.")
-                val passwordFileName = File(x.split("|")[2])
-
-                var choice = 0
-                while (choice < 3) {
-                    choice = getUserInput()
-
-                    if (choice == 1) {
-                        viewWebsites(passwordFileName)
-                        viewPasswords(passwordFileName)
-                    }
-                    else if (choice == 2) {
-                        addPassword(passwordFileName)
-                    }
-                    else {
-                        if (choice == 3){
-                            return
-                        }
-                        println("Invalid Choice")
-                        choice = 0
-                    }
+                print("Password: ")
+                val password = readLine()
+                if (password == x.split("|")[1]) {
+                    println("Login Successful.")
+                    return File(x.split("|")[2])
+                } else {
+                    println("Incorrect password")
+                    return File("FAILED")
                 }
-            } else {
-                println("Incorrect password")
             }
+        }
+        println("Username does not exist")
+        return File("FAILED")
+    }
 
+    // Session for user to view passwords or create new passwords
+    fun session(passwordFileName: File) {    // add timeout
+        var choice = 0
+        while (choice < 3) {
+            choice = getUserInput()
 
-
+            if (choice == 1) {
+                viewWebsites(passwordFileName)
+                viewPasswords(passwordFileName)
+            }
+            else if (choice == 2) {
+                addPassword(passwordFileName)
+            }
+            else {
+                if (choice == 3){
+                    return
+                }
+                println("Invalid Choice")
+                choice = 0
+            }
         }
     }
-    if (found == 0) {
-        println("Username does not exist")
-    }
+}
 
+fun main() {
+    // The user's filename
+    val userFileName = File("users.txt")
+
+    print("Enter 1 to login or 2 to create an account or anything else to quit: ")
+    val userInput = readLine()
+    if (userInput == "2"){
+        val user = User()
+        user.createAccount(userFileName)
+        val passwordFileName = user.login(userFileName)
+        if (passwordFileName != File("FAILED")) {
+            user.session(passwordFileName)
+        }
+
+    }
+    else if (userInput == "1") {
+        val user = User()
+        val passwordFileName = user.login(userFileName)
+        if (passwordFileName != File("FAILED")) {
+            user.session(passwordFileName)
+        }
+    } else {
+        println("Goodbye")
+        return
+    }
 
 }
